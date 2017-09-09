a12c0c3eb72e2501e60f8845a09c70c683ba6a32
alexwo <alexwo@gmail.com>
2017-09-09 14:02:29 +0300

    Update cfwizz.sh

--- scripts/cfwizz.sh
+++ scripts/cfwizz.sh
@@ -16,4 +16,4 @@ npm install nodemon
 npm install cfwizz
 npm install redbird
 echo -e "\e[40;38;5;82m Starting: CfWizz Agent ->>> \e[30;48;5;82m" Reloads code changes instantly \! "\033[0m"
-node ./node_modules/cfwizz/server.js
+node ./node_modules/cfwizz/boot_java.js
